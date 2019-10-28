99dd546666411f7fb689d5a720904120cd8803c1
YongminK <yongminni97@gmail.com>
2019-10-28 03:53:34 +0300

    первые два блока

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Landing from './Landing/Landing';
+import { Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <Container>
+      <Landing />
+    </Container>
+  );
+}
+
+export default App;
