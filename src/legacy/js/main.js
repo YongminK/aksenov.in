58f59d3fcfd9dd61dc54efaccec7f5b394c32393
Елена Петрова <e.petrova@int.robotbull.com>
2019-12-10 14:22:05 +0300

    updates

--- src/legacy/js/main.js
+++ src/legacy/js/main.js
@@ -13,62 +13,6 @@ var meny = Meny.create({
             touch: true
         });
 
-particlesJS('particles-js', {
-    "particles": {
-      "number": {
-        "value": 100,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#111"
-      },
-      "size": {
-        "value": 0,
-        "random": true,
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#006",
-        "opacity": 0.4,
-        "width": 2
-      },
-      "move": {
-        "enable": true,
-        "speed": 1,
-        "direction": "right",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-      },
-      nb: 80
-    },
-    "interactivity": {
-      "detect_on": "window",
-      "events": {
-        "onhover": {
-          "enable": false
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "resize": true
-      },
-      "modes": {
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
-        }
-      }
-    },
-    "retina_detect": true
-});
-
 $(".intro:first").children().hide().each(function(i){
   $(this).delay(i*2500).fadeIn(1000);
 });
