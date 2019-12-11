7b02f2240f7cfb0dab9629b8ac907bae8fdb5110
Елена Петрова <e.petrova@int.robotbull.com>
2019-12-11 20:14:44 +0300

    правильный билд возможно

---
+++ build/precache-manifest.ab10a1c6e6c98e42151d3e7b9081256b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33dedbb79cfa81f06f48c6763962e1a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d0942b190a2dba5e37db",
+    "url": "./static/css/2.116a8113.chunk.css"
+  },
+  {
+    "revision": "7bda4225a94027c74485",
+    "url": "./static/css/main.a7d49ae2.chunk.css"
+  },
+  {
+    "revision": "d0942b190a2dba5e37db",
+    "url": "./static/js/2.d81bd485.chunk.js"
+  },
+  {
+    "revision": "7bda4225a94027c74485",
+    "url": "./static/js/main.ffc9bb77.chunk.js"
+  },
+  {
+    "revision": "7b4a47fd6b93afee7393",
+    "url": "./static/js/runtime-main.502da2e8.js"
+  },
+  {
+    "revision": "b794284486645b2f8607502b45963245",
+    "url": "./static/media/0.b7942844.svg"
+  },
+  {
+    "revision": "087d2e3ec5cbb9d3d31228415c407eed",
+    "url": "./static/media/4.087d2e3e.svg"
+  },
+  {
+    "revision": "e20fe7c67901a9287f530c8924a05840",
+    "url": "./static/media/me.e20fe7c6.png"
+  },
+  {
+    "revision": "c006dcd5f36608365fee4e7f55909ca6",
+    "url": "./static/media/piglet.c006dcd5.png"
+  },
+  {
+    "revision": "7aa0c19ae8e35d351a70f4f8dbb8e011",
+    "url": "./static/media/rF_2XOmugvs.7aa0c19a.jpg"
+  }
+]);
