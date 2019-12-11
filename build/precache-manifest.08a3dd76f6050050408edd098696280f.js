0af79e69a1fc863456df7c4136255bb87a64f676
Елена Петрова <e.petrova@int.robotbull.com>
2019-12-11 19:50:31 +0300

    Create precache-manifest.08a3dd76f6050050408edd098696280f.js

---
+++ build/precache-manifest.08a3dd76f6050050408edd098696280f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3cac22159213b83b5438adb1f191b1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad81a9284cca0e7a5c63",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "a70680054b893b4c7976",
+    "url": "./static/css/main.eecf92a7.chunk.css"
+  },
+  {
+    "revision": "ad81a9284cca0e7a5c63",
+    "url": "./static/js/2.a50c707e.chunk.js"
+  },
+  {
+    "revision": "a70680054b893b4c7976",
+    "url": "./static/js/main.079f0613.chunk.js"
+  },
+  {
+    "revision": "7b4a47fd6b93afee7393",
+    "url": "./static/js/runtime-main.502da2e8.js"
+  },
+  {
+    "revision": "b794284486645b2f8607502b45963245",
+    "url": "./static/media/0.b7942844.svg"
+  },
+  {
+    "revision": "087d2e3ec5cbb9d3d31228415c407eed",
+    "url": "./static/media/4.087d2e3e.svg"
+  },
+  {
+    "revision": "7aa0c19ae8e35d351a70f4f8dbb8e011",
+    "url": "./static/media/rF_2XOmugvs.7aa0c19a.jpg"
+  }
+]);
