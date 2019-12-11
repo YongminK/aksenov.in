e9796a0a0a57ff43b9a53c1f4b585e2148275326
Елена Петрова <e.petrova@int.robotbull.com>
2019-12-12 02:39:51 +0300

    psth to 404

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
             <Switch>
                 <Route exact path='/' component={Main}/>
-                <Route exact path='/404' component={Cube}/>
+                <Route component={Cube}/>
             </Switch>
 
         </>
